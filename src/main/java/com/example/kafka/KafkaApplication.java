3d13279d94b0c68b0ce4254ec7c903197cae5efd
unknown <dolarte@trustingrisk.com.co>
2024-02-27 07:05:29 -0500

    Producer controller added

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -1,42 +1,82 @@
 package com.example.kafka;
 
+import com.example.kafka.model.Foo2;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
 
 @SpringBootApplication
 public class KafkaApplication {
 
+    private final Logger logger = LoggerFactory.getLogger(KafkaApplication.class);
+
+    private final TaskExecutor exec = new SimpleAsyncTaskExecutor();
+
     public static void main(String[] args) {
-        SpringApplication.run(KafkaApplication.class, args);
-        System.out.println("Kafka Springboot Service Started");
+        SpringApplication.run(KafkaApplication.class, args).close();
+    }
+
+    /*
+     * Boot will autowire this into the container factory.
+     */
+    @Bean
+    public CommonErrorHandler errorHandler(KafkaOperations<Object, Object> template) {
+        return new DefaultErrorHandler(
+                new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
+    }
+
+    @Bean
+    public RecordMessageConverter converter() {
+        return new JsonMessageConverter();
+    }
+
+    @KafkaListener(id = "fooGroup", topics = "topic1")
+    public void listen(Foo2 foo) {
+        logger.info("Received: " + foo);
+        if (foo.getFoo().startsWith("fail")) {
+            throw new RuntimeException("failed");
+        }
+        this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
+    }
+
+    @KafkaListener(id = "dltGroup", topics = "topic1.DLT")
+    public void dltListen(byte[] in) {
+        logger.info("Received from DLT: " + new String(in));
+        this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
     }
 
     @Bean
     public NewTopic topic() {
-        return TopicBuilder.name("topic1")
-                .partitions(10)
-                .replicas(1)
-                .build();
+        return new NewTopic("topic1", 1, (short) 1);
     }
 
-	/*@KafkaListener(id = "myId", topics = "topic1")
-	public void listen(String in) {
-		System.out.println(in);
-	}*/
+    @Bean
+    public NewTopic dlt() {
+        return new NewTopic("topic1.DLT", 1, (short) 1);
+    }
 
-   /* @Bean
-    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+    @Bean
+    @Profile("default") // Don't run from test(s)
+    public ApplicationRunner runner() {
         return args -> {
-            for (int i = 0; i < 100; i++) {
-                template.send("topic1", "from springboot"+i);
-            }
+            System.out.println("Hit Enter to terminate...");
+            System.in.read();
         };
-    }*/
-
+    }
 }
---
+++ src/main/java/com/example/kafka/controller/KafkaProduce.java
@@ -0,0 +1,21 @@
+package com.example.kafka.controller;
+
+import com.example.kafka.model.Foo1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaProduce {
+
+    @Autowired
+    private KafkaTemplate<Object, Object> template;
+
+    @PostMapping(path = "/send/foo/{what}")
+    public void sendFoo(@PathVariable String what) {
+        this.template.send("topic1", new Foo1(what));
+    }
+
+}
